9ac76858dce76f9ad0eccd54adaa8d717de536ba
Augustine-Sebastian <augustinesebastian007@gmail.com>
2024-11-04 14:51:28 +0530

    icons kinda fixed

--- home_automation/test/widget_test.dart
+++ home_automation/test/widget_test.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:home_automation/features/landing/presentation/components/my_textfield.dart';
 import 'package:home_automation/styles/colors.dart';
 import 'package:mockito/mockito.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Mock Firebase App for testing
 class MockFirebaseApp extends Mock implements FirebaseApp {}
